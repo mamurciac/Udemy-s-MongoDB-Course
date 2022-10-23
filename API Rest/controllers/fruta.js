66968ce98dd08bcafa70fcfa14903e8de2e8bfda
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-10-22 22:43:34 -0500

    Implementación del inserción de frutas (Método POST)

--- API Rest/controllers/fruta.js
+++ API Rest/controllers/fruta.js
@@ -1,11 +1,55 @@
 'use strict'
 
+var Fruta = require('../models/fruta');
+
 function pruebas(req, res){
   res.status(200).send({
     message: 'Esta ruta es de prueba en la API RESTful con MongoDB y NodeJS'
   });
 };
 
+function saveFruta(req, res){
+  var fruta = new Fruta();
+  var params = req.body;
+
+  if(params.nombre){
+    fruta.nombre = params.nombre;
+
+    if(params.color){
+      fruta.color = params.color;
+    }
+    if(params.temporada){
+      fruta.temporada = params.temporada;
+    }
+    if(params.peso){
+      fruta.peso = params.peso;
+    }
+
+    fruta.save((error, frutaStored) => {
+      if(error){
+        res.status(500).send({
+          message: error,
+        });
+      }else{
+        if(frutaStored){
+          res.status(200).send({
+            fruta: frutaStored
+          });
+        }else{
+          res.status(200).send({
+            message: 'No se ha guardado la fruta'
+          });
+        }
+      }
+    });
+  }else{
+    res.status(200).send({
+      message: 'El nombre de la fruta es obligatorio'
+    });
+  }
+};
+
 module.exports = {
-  pruebas
+  pruebas,
+  saveFruta
 };
--- API Rest/routes/fruta.js
+++ API Rest/routes/fruta.js
@@ -6,5 +6,6 @@ var FrutaController = require('../controllers/fruta');
 var api = express.Router();
 
 api.get('/pruebas', FrutaController.pruebas);
+api.post('/fruta', FrutaController.saveFruta);
 
 module.exports = api;
