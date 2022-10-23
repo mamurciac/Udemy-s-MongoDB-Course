5e01d7a9a46a758c2b01bab357299bede66acaf2
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-10-23 00:06:37 -0500

    Corrección de Conexión para tener permisos Lectura/Escritura

--- API Rest/index.js
+++ API Rest/index.js
@@ -4,12 +4,12 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = 3800;
 
-mongoose.connect('mongodb://localhost:27017/cursoMongoUdemy')
+mongoose.connect('mongodb://myTester:admin@localhost:27017/cursoMongoUdemy')
 .then(() => {
   console.log('La conexión se ha realizado correctamente');
 
   app.listen(port, () => {
-console.log('El servidor está corriendo en localhost:3800');
+    console.log('El servidor está corriendo en localhost:3800');
   });
 }).catch(error => {
   console.log(error);
--- Comandos para Instalación de Dependencias.sh
+++ Comandos para Instalación de Dependencias.sh
@@ -5,3 +5,9 @@ npm install body-parser --save
 npm install mongoose --save
 
 npm install nodemon --save-dev
+
+
+
+
+#use cursoMongoUdemy;
+#db.createUser({user: 'myTester', pwd: 'admin', roles: [{role: "readWrite", db: "cursoMongoUdemy"}]});
